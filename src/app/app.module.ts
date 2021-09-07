cf138d0acf4ff3b641dbff58931ac9847c498760
benevolenskiartour <benevolenskiartour@gmail.com>
2021-09-07 15:01:47 -0400

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
+import {ProductAlertsComponent} from './product-alerts/product-alerts.component';
+
 @NgModule({
   imports: [
     BrowserModule,
